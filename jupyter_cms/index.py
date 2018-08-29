76c1607642b8c5dbd4e5683e13961666b812c407
Leo Gallucci <leo.gallucci@zalando.de>
2018-08-29 21:49:39 +0200

    Fix line ending formatting

--- jupyter_cms/index.py
+++ jupyter_cms/index.py
@@ -24,36 +24,36 @@ class Index(object):
         work_dir = '/home/nbuser'
         self.work_dir = work_dir
         self._init_index()
-        
+
     def _init_index(self, reset=False):
         index_path = os.path.join(jupyter_data_dir(), 'index')
-        
+
         # clear out old index if requested
         if reset:
             shutil.rmtree(index_path, True)
-        
+
         # make sure there's a path to store the index data
         if not os.path.exists(index_path):
             os.makedirs(index_path)
-            
+
         if not exists_in(index_path):
             # create an index with the current schema
-            schema = Schema(basename=TEXT(stored=True, field_boost=5.0), 
+            schema = Schema(basename=TEXT(stored=True, field_boost=5.0),
                             dirname=ID(stored=True),
-                            path=ID(stored=True, unique=True), 
-                            content=TEXT(stored=False), 
+                            path=ID(stored=True, unique=True),
+                            content=TEXT(stored=False),
                             time=STORED)
             self.ix = create_in(index_path, schema)
         else:
             # open the existing index
             self.ix = open_dir(index_path)
-            
+
         # build a query parser based on the current schema
         self.query_parser = MultifieldParser(["content", "basename", "dirname"], self.ix.schema)
-    
+
     def _file_to_document(self, filename, m_time):
         content = u''
-        
+
         # get content for notebooks only
         if filename.endswith('.ipynb'):
             with io.open(filename, 'r', encoding='utf-8') as f:
@@ -69,7 +69,7 @@ class Index(object):
             content=content,
             time=m_time
         )
-    
+
     def _scan_disk(self, on_disk, path):
         for entry in scandir(path):
             if not entry.name.startswith('.') and entry.is_dir():
@@ -77,14 +77,14 @@ class Index(object):
             elif entry.is_file():
                 on_disk[entry.path] = entry.stat().st_mtime
         return on_disk
-                
+
     def _scan_index(self):
         in_index = {}
         with self.ix.searcher() as searcher:
             for fields in searcher.all_stored_fields():
                 in_index[fields['path']] = fields['time']
         return in_index
-                
+
     def _compute_ops(self, on_disk, in_index):
         in_index_set = set(in_index.keys())
         on_disk_set = set(on_disk.keys())
@@ -92,18 +92,18 @@ class Index(object):
         to_remove = in_index_set - on_disk_set
         to_add = on_disk_set - in_index_set
         to_update = on_disk_set.intersection(in_index_set)
-        
+
         return to_add, to_remove, to_update
-        
+
     def _add_to_index(self, writer, to_add, on_disk):
         for filename in to_add:
             meta = self._file_to_document(filename, on_disk[filename])
             writer.add_document(**meta)
-        
+
     def _remove_from_index(self, writer, to_remove):
         for filename in to_remove:
             writer.delete_by_term('path', filename)
-            
+
     def _update_in_index(self, writer, to_update, on_disk, in_index):
         for filename in to_update:
             # only update if modification time differs
@@ -111,7 +111,7 @@ class Index(object):
                 continue
             meta = self._file_to_document(filename, on_disk[filename])
             writer.update_document(**meta)
-    
+
     def update_index(self):
         '''
         Updates the index based on the disk/index delta.
@@ -120,9 +120,9 @@ class Index(object):
         on_disk = self._scan_disk(on_disk, self.work_dir)
         in_index = self._scan_index()
         to_add, to_remove, to_update = self._compute_ops(on_disk, in_index)
-        
-        #print(len(to_add), len(to_remove), len(to_update)) 
-        
+
+        #print(len(to_add), len(to_remove), len(to_update))
+
         try:
             writer = self.ix.writer()
         except LockError:
